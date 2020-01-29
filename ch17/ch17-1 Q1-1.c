8961ebbbd38480bccbb7dbea25ffb910ec26b40d
ekdms342 <ekdms342@gmail.com>
2020-01-29 17:27:36 +0900

    2020.01.29

---
+++ ch17/ch17-1 Q1-1.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+void MinMax(int* arr, int** mindp, int** maxdp)
+{
+	int i;
+	*mindp = &arr[0];
+	*maxdp = &arr[0];
+
+	for (i = 1; i < 5; i++)
+	{
+		if (*(*mindp) > arr[i])
+		{
+			*mindp = &arr[i];
+		}
+		if (*(*maxdp) < arr[i])
+		{
+			*maxdp = &arr[i];
+		}
+	}
+}
+int main(void)
+{
+	int arr[5] = { 2, 4, 7, 1, 6 };
+	int* minptr;
+	int* maxptr;
+	MinMax(arr, &minptr, &maxptr);
+	printf("%d %d", *minptr, *maxptr);
+}
