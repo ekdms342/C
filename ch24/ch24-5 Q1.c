db77b54a331f5afd1dd80e3a3506674a27bb4207
ekdms342 <ekdms342@gmail.com>
2020-03-04 22:47:16 +0900

    2020.03.04

---
+++ ch24/ch24-5 Q1.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+int byte(FILE* num )
+{
+	long fpos = ftell(num);
+	long size;
+
+	fseek(num, 0, SEEK_END);
+	size = ftell(num);
+
+	fseek(num, fpos, SEEK_SET);
+	return size;
+}
+
+int main(void)
+{
+	long fpos;
+	FILE* fp = fopen("test.txt", "wt");
+	fputs("ABCDEFGH", fp);
+	
+	fseek(fp, -2, SEEK_END);
+	fpos = ftell(fp);
+	printf("%d \n", fpos);
+	
+	printf("파일크기: %d \n", byte(fp));
+	fpos = ftell(fp);
+	printf("%d \n", fpos);
+	
+}
